898b865633516167181ec2e8ceb93e3c06533c80
Ashitapv <ashitapiyush21@gmail.com>
2022-12-20 22:51:57 +0530

    props_video_path

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <video width="250" height="250" controls >
+      <source src="../Videos/1.mp4" type="video/mp4"/>
+      {/* my-app/public/Videos/video1.mp4 */}
+     </video>
+    </div>
+  );
+}
+export default App;
